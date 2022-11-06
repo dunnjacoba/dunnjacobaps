8a6d1da6fa44a0cd4a7c4ada9f0742296db355a3
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-11-05 23:38:32 -0500

    fix: content

--- lib/posts.ts
+++ lib/posts.ts
@@ -47,7 +47,7 @@ export async function getPostData(id: string) {
   const matterResult = matter(fileContents)
   const processedContent = await remark()
     .use(html)
-    .process(matterResult.content)
+    .process(matterResult?.content)
   const contentHtml = processedContent.toString()
 
   return {
